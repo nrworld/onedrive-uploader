3c9880ded154eb0a91299a189e7634d5bfd45a8c
Nikhil Raj <nikhil.raj@dealer.com>
2015-10-20 16:37:59 -0400

    first commit

---
+++ src/main/java/com/rajonweb/UploaderMain.java
@@ -0,0 +1,44 @@
+package com.rajonweb;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.WatchService;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by ddcnikhilr on 10/20/15.
+ */
+public class UploaderMain {
+    public static void main(String[] args) {
+        UploaderMain um = new UploaderMain();
+        Properties app_config = um.getConfig();
+        if (null == app_config) {
+            System.out.println("Error reading config");
+            return;
+        }
+        try {
+            List<String> watched_dirs = Arrays.asList(app_config.getProperty("watched_dirs").split(":"));
+
+
+            WatchService watcher = FileSystems.getDefault().newWatchService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Properties getConfig() {
+        Properties app_config = new Properties();
+        ClassLoader classLoader = getClass().getClassLoader();
+        try {
+            app_config.load(classLoader.getResourceAsStream("app.cfg"));
+            return app_config;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
